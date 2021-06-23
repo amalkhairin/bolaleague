2f355dc15dca9b641059f82b3d2d1e1146b854bd
amalkhairin <amallanch45@gmail.com>
2021-06-23 14:55:48 +0700

    update router

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
 const userRouter = require('../routes/user.route');
+const groupRouter = require('./group.route');
+const matchRouter = require('../routes/matches.route');
+
 router.use('/v1/user', userRouter);
+router.use('/v1/group', groupRouter);
+router.use('/v1/matches', matchRouter);
 
 module.exports = router;
