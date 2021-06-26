b61dd76fc8ae4f024a426d29d6a70441594d38f2
amalkhairin <amallanch45@gmail.com>
2021-06-26 11:18:55 +0700

    fix db

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-        host: 'remotemysql.com',
-        user: 'TkFSG21ulZ',
-        password: 'qzXIrrlJDa',
-        database: 'TkFSG21ulZ',
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
     },
     pool: {min: 0, max: 50}
 })
